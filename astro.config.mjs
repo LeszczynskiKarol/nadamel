46282ade5deab8f542caade4986fa688dde1ba44
LeszczynskiKarol <leszczu123@wp.pl>
2026-02-11 13:11:01 +0100

    poprawka seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nadamel.pl',
-  integrations: [
-    tailwind(),
-    sitemap()
-  ],
+  site: "https://www.nadamel.pl",
+  integrations: [tailwind(), sitemap()],
   compressHTML: true,
   build: {
-    inlineStylesheets: 'auto'
-  }
+    inlineStylesheets: "auto",
+  },
 });
